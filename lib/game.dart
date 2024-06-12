91742b9be7c9b2e0a3d2eb983d6972a61742ced6
Sarwat Aijaz <sarwataijaaz@gmail.com>
2024-06-12 22:19:41 +0500

    grid layout

--- lib/game.dart
+++ lib/game.dart
@@ -12,86 +12,150 @@ class GameLogic extends StatefulWidget {
 }
 
 class _GameLogicState extends State<GameLogic> {
+
+  late String currentPlayer;
+  late String player1;
+  late String player2;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize currentPlayer with widget.name1
+    currentPlayer = widget.name1;
+    player1 = widget.name1;
+    player2 = widget.name2;
+  } //
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple,
-      body: GridView.count(
-        crossAxisCount: 2,
-        crossAxisSpacing: 20,
-        padding: EdgeInsets.all(30.0),
+      body: Column(
         children: [
-          Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color(0xFF000080)),
-            child: Column(
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(widget.name1,
-                    style: TextStyle(
-                        fontSize: 23,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'HoneyCrepes',
-                        color: Colors.white)),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Icon(Icons.close, color: Colors.red, size: 45),
-                )
-              ],
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color(0xFF000080)),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(widget.name2,
-                    style: TextStyle(
-                        fontSize: 23,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'HoneyCrepes',
-                        color: Colors.white)),
+                  child: Container(
+                    width: 150,
+                    height: 150,
+                    padding: EdgeInsets.only(right: 10.0),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF000080)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(widget.name1,
+                            style: TextStyle(
+                                fontSize: 23,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'HoneyCrepes',
+                                color: Colors.white)),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(Icons.close, color: Colors.red, size: 48),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Icon(Icons.circle_outlined,
-                      color: Colors.yellow, size: 45),
+                  child: Container(
+                    width: 150,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF000080)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(widget.name2,
+                            style: TextStyle(
+                                fontSize: 23,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'HoneyCrepes',
+                                color: Colors.white)),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(Icons.circle_outlined,
+                              color: Colors.yellow, size: 45),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
           ),
-          // Padding(
-          //   padding: EdgeInsets.only(top: 50, left: 5.0, right: 5.0),
-          //   child: Container(
-          //     decoration: BoxDecoration(
-          //         borderRadius: BorderRadius.circular(10),
-          //         color: Color(0xFF000080)),
-          //     child: GridView.count(
-          //       crossAxisCount: 3,
-          //       crossAxisSpacing: 20,
-          //       padding: EdgeInsets.all(30.0),
-          //       children: [
-          //         Container(
-          //           decoration: BoxDecoration(
-          //               borderRadius: BorderRadius.circular(10),
-          //               color: Color(0xFF000080)),
-          //         ),
-          //         Container(
-          //           decoration: BoxDecoration(
-          //               borderRadius: BorderRadius.circular(10),
-          //               color: Color(0xFF000080)),
-          //         ),
-          //         Container(
-          //           decoration: BoxDecoration(
-          //               borderRadius: BorderRadius.circular(10),
-          //               color: Color(0xFF000080)),
-          //         ),
-          //       ],
-          //     ),
-          //   ),
-          // )
+          Padding(
+            padding: EdgeInsets.only(top: 50, left: 5.0, right: 5.0),
+            child: Container(
+              height: 380,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.blueAccent,
+               ),
+              child: GridView.count(
+                crossAxisCount: 3,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                padding: EdgeInsets.all(15.0),
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF000080)),
+
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF000080)),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF000080)),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF000080)),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF000080)),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF000080)),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF000080)),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF000080)),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF000080)),
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
