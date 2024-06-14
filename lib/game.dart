f247dc2663a16fff01b87f3b954c895a00ee26dc
Sarwat Aijaz <sarwataijaaz@gmail.com>
2024-06-14 06:11:47 +0500

    player logic

--- lib/game.dart
+++ lib/game.dart
@@ -17,6 +17,26 @@ class _GameLogicState extends State<GameLogic> {
   late String player1;
   late String player2;
 
+  bool isTap1 = false;
+  bool isTap2 = false;
+  bool isTap3 = false;
+  bool isTap4 = false;
+  bool isTap5 = false;
+  bool isTap6 = false;
+  bool isTap7 = false;
+  bool isTap8 = false;
+  bool isTap9 = false;
+
+  static bool state1 = true;
+  static bool state2 = true;
+  static bool state3 = true;
+  static bool state4 = true;
+  static bool state5 = true;
+  static bool state6 = true;
+  static bool state7 = true;
+  static bool state8 = true;
+  static bool state9 = true;
+
   @override
   void initState() {
     super.initState();
@@ -57,7 +77,7 @@ class _GameLogicState extends State<GameLogic> {
                                 color: Colors.white)),
                         Padding(
                           padding: const EdgeInsets.all(8.0),
-                          child: Icon(Icons.close, color: Colors.red, size: 48),
+                          child: Icon(Icons.close, color: Colors.red, size: 45),
                         )
                       ],
                     ),
@@ -97,67 +117,106 @@ class _GameLogicState extends State<GameLogic> {
             child: Container(
               height: 380,
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.blueAccent,
-               ),
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.blueAccent,
+              ),
               child: GridView.count(
-                crossAxisCount: 3,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                padding: EdgeInsets.all(15.0),
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Color(0xFF000080)),
-
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Color(0xFF000080)),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Color(0xFF000080)),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Color(0xFF000080)),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Color(0xFF000080)),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Color(0xFF000080)),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Color(0xFF000080)),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Color(0xFF000080)),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Color(0xFF000080)),
-                  ),
-                ],
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 5,
+                  mainAxisSpacing: 5,
+                  padding: EdgeInsets.all(15.0),
+                  children: [
+                    buildInkWellWidget(isTap1, state1, 1, currentPlayer),
+                    buildInkWellWidget(isTap2, state2, 2, currentPlayer),
+                    buildInkWellWidget(isTap3, state3, 3, currentPlayer),
+                    buildInkWellWidget(isTap4, state4, 4, currentPlayer),
+                    buildInkWellWidget(isTap5, state5, 5, currentPlayer),
+                    buildInkWellWidget(isTap6, state6, 6, currentPlayer),
+                    buildInkWellWidget(isTap7, state7, 7, currentPlayer),
+                    buildInkWellWidget(isTap8, state8, 8, currentPlayer),
+                    buildInkWellWidget(isTap9, state9, 9, currentPlayer),
+                  ]
               ),
             ),
           )
         ],
       ),
     );
+  }
+
+  //  you are passing a reference to the actual isTap1 variable in memory, not a copy of its value. Therefore, any changes made to isTap1 within that method or widget will reflect the changes to the original isTap1 variable.
+  Widget buildInkWellWidget(bool isTap, bool state, int index, String player) {
+
+
+    return InkWell(
+      child: AnimatedContainer(
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Color(0xFF000080),
+        ),
+        duration: Duration(milliseconds: 100),
+        curve: Curves.easeInOut,
+        child: AnimatedSwitcher(
+          duration: Duration(milliseconds: 300),
+          transitionBuilder: (Widget child, Animation<double> animation) {
+            return ScaleTransition(child: child, scale: animation);
+          },
+          child: Icon(
+            key: ValueKey<bool>(isTap),
+            (player == player1) ? Icons.close : Icons.circle_outlined,
+            color: isTap ? ((player == player1) ? Colors.red : Colors.yellow) : Color(0xFF000080),
+            size: 70,
+          ),
+        ),
+      ),
+      onTap: () {
+        print("onTap triggered for widget $index, state=$state, isTap=$isTap");
+        if (state) {
+          print("Widget $index Pressed!");
+          setState(() {
+            switch (index) {
+              case 1:
+                isTap1 = !isTap1;
+                state1 = false;
+                break;
+              case 2:
+                isTap2 = !isTap2;
+                state2 = false;
+                break;
+              case 3:
+                isTap3 = !isTap3;
+                state3 = false;
+                break;
+              case 4:
+                isTap4 = !isTap4;
+                state4 = false;
+                break;
+              case 5:
+                isTap5 = !isTap5;
+                state5 = false;
+                break;
+              case 6:
+                isTap6 = !isTap6;
+                state6 = false;
+                break;
+              case 7:
+                isTap7 = !isTap7;
+                state7 = false;
+                break;
+              case 8:
+                isTap8 = !isTap8;
+                state8 = false;
+                break;
+              case 9:
+                isTap9 = !isTap9;
+                state9 = false;
+                break;
+            }
+          });
+        }
+      },
+    );
+
   }
 }
